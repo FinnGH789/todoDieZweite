142c09f2dd4371f6ae4265fef6862860e7358e42
Finnb <finnb@freenet.de>
2024-01-17 22:28:16 +0100

    analytics template built

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { FormComponent } from './ToDo/form/form.component';
 import { InputComponent } from './input/input.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { AnalyticsComponent } from './ToDo/analytics/analytics.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, FormComponent, InputComponent, ReactiveFormsModule],
+  imports: [CommonModule,AnalyticsComponent , RouterOutlet, FormComponent, InputComponent, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
